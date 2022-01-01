31666b5a6252ec1f3d6d00172b679b0414e4ae93
retchut <retchut@protonmail.ch>
2022-01-01 23:03:44 +0000

    chore: update page template

--- webapp/code/resources/views/layouts/app.blade.php
+++ webapp/code/resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Styles -->
     <!-- <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet"> -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
@@ -23,27 +23,35 @@
   </head>
   <body>
     <main>
-      <header class="container-fluid">
-        <h1><a class="title" href="{{ url('/') }}">Event Pointer</a></h1>
-        <div>
-          <a class="button" href="{{ url('/browse') }}">Browse</a>
-          <a class="button" href="{{ url('/member') }}">My Page</a>
-          <!-- if authenticated, shows logout button -->
-          @if (Auth::check())
-          <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
-          @else
-          <a class="button" href="{{ url('/login') }}"> Log In </a>
-          @endif
+    <header class="container-fluid">
+        <div class="row">
+          <div class="col-sm-8">
+            <h1><a class="title" href="/">Event Pointer</a></h1>
+          </div>
+          <div class="col d-flex align-items-center">
+            <div class="col">
+              <a class="button" href="/">Browse</a>
+            </div>
+            <div class="col">
+              <a class="button" href="/">My Page</a>
+            </div>
+            <div class="col">
+              <a class="button" href="/"> Log In </a>
+            </div>
+          </div>
         </div>
       </header>
-      <section id="content">
-        @yield('content')
-      </section>
       <footer class="container-fluid">
-        <div><a class="text" href="{{ url('/about') }}">About</a></div>
-        <div>
-          <a class="text" href="{{ url('/contacts') }}">Contacts</a>
-          <a class="text" href="{{ url('/faq') }}">FAQ</a>
+        <div class="row">
+          <div class="col-sm-10">
+            <a class="text" href="/">About</a>
+          </div>
+          <div class="col-sm-1">
+            <a class="text" href="/">Contacts</a>
+          </div>
+          <div class="col-sm-1">
+            <a class="text" href="/">FAQ</a>
+          </div>
         </div>
       </footer>
     </main>

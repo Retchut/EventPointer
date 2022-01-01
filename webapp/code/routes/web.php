0d0c96d1cf48423bea1323d6f6cafd0ee26688d8
Carlos Gomes <up201906622@edu.fe.up.pt>
2022-01-01 21:01:42 +0000

    Change routes

--- webapp/code/routes/web.php
+++ webapp/code/routes/web.php
@@ -10,20 +10,64 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 // Home
-// Route::get('/', 'Auth\LoginController@home');
-Route::get('/', 'HomeController@show');
+Route::get('home', /*homecontroller*/);
 
-// Cards
-Route::get('cards', 'CardController@list');
-Route::get('cards/{id}', 'CardController@show');
 
-// API
-Route::put('api/cards', 'CardController@create');
-Route::delete('api/cards/{card_id}', 'CardController@delete');
-Route::put('api/cards/{card_id}/', 'ItemController@create');
-Route::post('api/item/{id}', 'ItemController@update');
-Route::delete('api/item/{id}', 'ItemController@delete');
+// Authentication
+Route::get('login', /*login form*/);
+Route::post('login', /*login controller*/);
+Route::post('logout', /*logout controller*/);
+Route::get('register', /*register form*/);
+Route::post('register', /*register controller*/);
+
+
+// Indiviual Profile
+Route::get('user/{user_id}', /*view user profile controller*/);
+Route::get('user/{user_id}/edit', /*user edit page form controller*/);
+
+
+// Event
+Route::get('event/{event_id}', /*view event controller*/);
+Route::get('user/{user_id}/create_event', /*create event form*/);
+Route::post('user/{user_id}/create_event', /*create event controller*/);
+Route::get('event/{id}/edit', /*edit event form*/);
+Route::post('event/{event_id}/edit', /*edit event controller*/);
+Route::get('event/{event_id}/', /*delete event controller*/);
+Route::get('event/{event_id}/invite', /*invite to event form*/);
+Route::post('event/{event_id}/invite', /*invite to event controller*/);
+Route::get('user/{user_id}/invite/{invite_id}', /*invitation form*/);
+Route::post('user/{user_id}/invite/{invite_id}', /*invitation controler*/);
+Route::get('event/{event_id}/participants',/*participants controller*/);
+Route::get('event/{event_id}/participants/remove',/*delete participant controller*/);
+
+//API
+
+Route::get('api/events',/*search events controller*/);
+Route::get('api/users',/*search users controller*/);
+
+
+// Event Participation and Ownership
+Route::get('user/{user_id}/my_events',/*view my events controller*/);
+Route::get('user/{user_id}/my_participations',/*view my participations controller*/);
+
+//Announcements and Comments
+// TODO
+
+// Static pages
+Route::get('faq', /*FAQ CONTROLLER*/);
+Route::get('about', /*about CONTROLLER*/);
+Route::get('contacts', /*contacts CONTROLLER*/);
+
+
+// Admin
+Route::get('admin/user/{user_id}/delete', /*delete user controller*/);
+Route::get('admin/create_report',/*create report form*/);
+Route::post('admin/create_report',/*create report form*/);
+Route::get('admin/report/{report_id}/delete',/*delete report controller*/);
+
+
 
 // Authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');

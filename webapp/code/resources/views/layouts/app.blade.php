30158ded1ef48f93f0fcde1f0dd8eb4407814d62
retchut <retchut@protonmail.ch>
2022-01-01 20:40:24 +0000

    feat: finish simple page layout

--- webapp/code/resources/views/layouts/app.blade.php
+++ webapp/code/resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet"> -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
@@ -22,24 +23,28 @@
   </head>
   <body>
     <main>
-      <header>
-        <h1><a href="{{ url('/home') }}">Event Pointer</a></h1>
-        <a class="button" href="{{ url('/browse') }}">browse</a>
-        <a class="button" href="{{ url('/member') }}">my page</a>
-        <!-- if authenticated, shows logout button -->
-        @if (Auth::check())
-        <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
-        @else
-        <a class="button" href="{{ url('/login') }}"> Log in </a>
-        @endif
+      <header class="container-fluid">
+        <h1><a class="title" href="{{ url('/') }}">Event Pointer</a></h1>
+        <div>
+          <a class="button" href="{{ url('/browse') }}">Browse</a>
+          <a class="button" href="{{ url('/member') }}">My Page</a>
+          <!-- if authenticated, shows logout button -->
+          @if (Auth::check())
+          <a class="button" href="{{ url('/logout') }}"> Logout </a> <span>{{ Auth::user()->name }}</span>
+          @else
+          <a class="button" href="{{ url('/login') }}"> Log In </a>
+          @endif
+        </div>
       </header>
       <section id="content">
         @yield('content')
       </section>
-      <footer>
-        <a class="button" href="{{ url('/about') }}">about</a>
-        <a class="button" href="{{ url('/contacts') }}">contacts</a>
-        <a class="button" href="{{ url('/faq') }}">FAQ</a>
+      <footer class="container-fluid">
+        <div><a class="text" href="{{ url('/about') }}">About</a></div>
+        <div>
+          <a class="text" href="{{ url('/contacts') }}">Contacts</a>
+          <a class="text" href="{{ url('/faq') }}">FAQ</a>
+        </div>
       </footer>
     </main>
   </body>

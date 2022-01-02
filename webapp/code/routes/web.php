e0327a7627adf633f0beed505df69c7bf5548658
retchut <retchut@protonmail.ch>
2022-01-02 03:17:04 +0000

    feat: add simple routes

--- webapp/code/routes/web.php
+++ webapp/code/routes/web.php
@@ -11,11 +11,49 @@
 |
 */
 
-// Home
+// Pages
 Route::get('/', 'HomeController@show');
-//Route::get('/home', 'HomeController@show');
+Route::get('about', 'AboutController@show');
+Route::get('faq', 'FAQController@show');
+Route::get('contacts', 'ContactsController@show');
+Route::get('browse', 'BrowseController@show');
 
+// User Profile
+Route::get('user/{user_id}', 'UserController@show');
 
+//User Edit
+Route::get('user/{user_id}/edit', /*'UserController@edit'*/);
+/* Cria-se novo controller para uma nova página para editar, onde se faz depois a edição, e aí sim, chama-se o método edit? */
+
+//User Create Event
+Route::get('user/{user_id}/create_event', /*create event form*/);
+Route::post('user/{user_id}/create_event', /*create event controller*/);
+
+// User Ivite
+Route::get('user/{user_id}/invite/{invite_id}', /*invitation form*/);
+Route::post('user/{user_id}/invite/{invite_id}', /*invitation controler*/);
+
+// User Events
+Route::get('user/{user_id}/my_events',/*view my events controller*/);
+
+// User Participations
+Route::get('user/{user_id}/my_participations',/*view my participations controller*/);
+
+// Event
+Route::get('event/{event_id}', 'EventController@show');
+Route::delete('event/{event_id}/', 'EventController@delete'); /* Missing logic */
+
+// Event Edit
+Route::get('event/{event_id}/edit', /*edit event form*/);
+Route::post('event/{event_id}/edit', /*edit event controller*/);
+
+// Event Invite
+Route::get('event/{event_id}/invite', /*invite to event form*/);
+Route::post('event/{event_id}/invite', /*invite to event controller*/);
+
+// Event Participants
+Route::get('event/{event_id}/participants',/*participants controller*/);
+Route::delete('event/{event_id}/participants/{participant_id}',/*delete participant controller*/);
 
 // Authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
@@ -25,47 +63,24 @@ Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('re
 Route::post('register', 'Auth\RegisterController@register');
 
 
-// Indiviual Profile
-Route::get('user/{user_id}', /*view user profile controller*/);
-Route::get('user/{user_id}/edit', /*user edit page form controller*/);
-
-
-// Event
-Route::get('event/{event_id}', /*view event controller*/);
-Route::get('user/{user_id}/create_event', /*create event form*/);
-Route::post('user/{user_id}/create_event', /*create event controller*/);
-Route::get('event/{id}/edit', /*edit event form*/);
-Route::post('event/{event_id}/edit', /*edit event controller*/);
-Route::delete('event/{event_id}/', /*delete event controller*/);
-Route::get('event/{event_id}/invite', /*invite to event form*/);
-Route::post('event/{event_id}/invite', /*invite to event controller*/);
-Route::get('user/{user_id}/invite/{invite_id}', /*invitation form*/);
-Route::post('user/{user_id}/invite/{invite_id}', /*invitation controler*/);
-Route::get('event/{event_id}/participants',/*participants controller*/);
-Route::delete('event/{event_id}/participants/{participant_id}',/*delete participant controller*/);
-
 //API
-
 Route::get('api/events',/*search events controller*/);
 Route::get('api/users',/*search users controller*/);
-
-
-// Event Participation and Ownership
-Route::get('user/{user_id}/my_events',/*view my events controller*/);
-Route::get('user/{user_id}/my_participations',/*view my participations controller*/);
 
 //Announcements and Comments
 // TODO
 
-// Static pages
-Route::get('faq', /*FAQ CONTROLLER*/);
-Route::get('about', /*about CONTROLLER*/);
-Route::get('services', /*services CONTROLLER*/);
-Route::get('contacts', /*contacts CONTROLLER*/);
+// Admin Profile
+Route::get('admin/{admin_id}', 'AdminController@show');
 
-
-// Admin
-Route::get('admin/user/{user_id}/delete', /*delete user controller*/);
+// Admin Create Report
 Route::get('admin/create_report',/*create report form*/);
 Route::post('admin/create_report',/*create report form*/);
+
+// Admin manage user
+Route::get('admin/user/{user_id}/', /* admin used edit page contoller */);
+Route::get('admin/user/{user_id}/delete', /*delete user controller*/);
+
+// Admin report
+Route::get('admin/report/{report_id}/',/* see reports controller*/);
 Route::get('admin/report/{report_id}/delete',/*delete report controller*/);

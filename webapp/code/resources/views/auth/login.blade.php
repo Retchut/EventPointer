226505de3009cec41c038a705bab17f590a033b6
retchut <retchut@protonmail.ch>
2022-01-03 01:37:40 +0000

    feat: style login screen

--- webapp/code/resources/views/auth/login.blade.php
+++ webapp/code/resources/views/auth/login.blade.php
@@ -1,32 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
 
-    <label for="email">E-mail</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-    @if ($errors->has('email'))
-        <span class="error">
-          {{ $errors->first('email') }}
-        </span>
-    @endif
+<section id="login">
+    <h1 class="pt-4 px-4">Log In</h1>
+    <form method="POST" action="{{ route('login') }}">
+        {{ csrf_field() }}
+        <div class="row p-4">
+            <div class="col-sm-6 m-4 text-boxes">
+                <div class="row">
+                    <label for="email">E-mail</label>
+                    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+                    @if ($errors->has('email'))
+                        <span class="error">
+                        {{ $errors->first('email') }}
+                        </span>
+                    @endif
+                </div>
 
-    <label for="password" >Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-        <span class="error">
-            {{ $errors->first('password') }}
-        </span>
-    @endif
+                <div class="row">
+                <label for="password" >Password</label>
+                    <input id="password" type="password" name="password" required>
+                    @if ($errors->has('password'))
+                        <span class="error">
+                            {{ $errors->first('password') }}
+                        </span>
+                    @endif
+                </div>
+            </div>
 
-    <label>
-        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-    </label>
+            <div class="col-sm-4 m-4 buttons">
+                <div class="row">
+                    <label>
+                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                    </label>
+                </div>
 
-    <button type="submit">
-        Login
-    </button>
-    <a class="button button-outline" href="{{ route('register') }}">Register</a>
-</form>
+                <div class="row">
+                    <button type="submit button">
+                        Login
+                    </button>
+                </div>
+
+                <div class="row">
+                    <a class="button button-outline" href="{{ route('register') }}">Register</a>
+                </div>
+
+            </div>
+        </div>
+    </form>
+</section>
 @endsection

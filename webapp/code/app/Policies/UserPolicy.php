d3df30c539997852ef9db87086d94f94f08b2699
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2022-01-19 15:28:35 +0000

    create event in the making

--- webapp/code/app/Policies/UserPolicy.php
+++ webapp/code/app/Policies/UserPolicy.php
@@ -12,10 +12,11 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    /*
     public function delete(User $user1, User $user2)
     {
         return $user1->id == $user2->id || $user1->admin;
-    }
+    }*/
 
     public function create(User $user)
     {

9488de4511394fdd616ff41be5552dca2a230247
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2022-01-21 13:23:48 +0000

    poll changes

--- webapp/code/resources/views/pages/editcomment.blade.php
+++ webapp/code/resources/views/pages/editcomment.blade.php
@@ -11,7 +11,7 @@
                     <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                   </svg></a>Edit Comment
         </h1>
-        <form id="comment_form" method="POST" action="{{ route('comment.update', ['comment_id' => $comment_id, 'comment' => $comment]) }}">
+        <form id="comment_form" method="POST" action="{{ route('comment.update', ['event_id' => $event_id, 'comment' => $comment]) }}">
             {{ csrf_field() }}
             <div class="form-group mt-3">
                 <label for="comment">Please rewrite your message in this box...</label>
